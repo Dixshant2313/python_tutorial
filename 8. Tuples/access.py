fcf3f4894873abb10bb22a3feefc40f94c87fcd5
Dixshant Charan <dixshantcharan2313@gmail.com>
2025-07-23 00:49:27 +0530

    Tuples in Python

---
+++ 8. Tuples/access.py
@@ -0,0 +1,15 @@
+a = (1,2,3)
+
+"""
+a[0] = 100
+print(a)        Will give an error as the elements inside a tuple cannot be modified
+"""
+
+#slicing a tuple
+print(a[0:3:1])
+
+print(max(a))
+print(min(a))
+
+# These are used to store the data which does not require any change in the future
+# for example:- DOB, Gender, UID
---
+++ 8. Tuples/tuple.py
@@ -0,0 +1,15 @@
+a = (10,20,30)
+b = ()              #empty tuple
+print(a, b)
+
+# methods that can be used on tuples
+# len()
+# min()
+# max()
+# count()
+# index
+
+c,d,e,f = (1,2,3,4)  #tuple unpacking
+print(c,d,e,f)
+
+
